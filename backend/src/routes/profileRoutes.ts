8da58b841d5a179d7661f940fbd6a5bc5c42fae6
HARI HARA SUDHAN K <hhsudhan3333@gmail.com>
2025-09-18 17:50:38 +0530

    Delete backend directory

--- backend/src/routes/profileRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authMiddleware } from "../middlewares/authMiddleware";
-import { updateProfile, getProfile } from "../controllers/profileController";
-
-const router = express.Router();
-
-router.get("/profile", authMiddleware, getProfile);
-router.put("/update/profile", authMiddleware, updateProfile);
-export default router;
